eba9461951998f1c8cf832ca7819214ea6b3d3c3
IDilettant <dedenev.dev@gmail.com>
2026-01-06 20:26:44 +0100

    feat(api): init sentry and read port from env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 
 	"code/internal/web"
 )
 
-const port = ":8080"
+const (
+	defaultPort   = "8080"
+	sentryTimeout = 2 * time.Second
+)
 
 func main() {
+	cleanup, err := initSentry()
+	if err != nil {
+		log.Printf("Sentry disabled: %v", err)
+	}
+	defer cleanup()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
 	r := web.NewRouter()
 
-	err := r.Run(port)
+	err = r.Run(port)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initSentry() (func(), error) {
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn == "" {
+		return func() {}, errors.New("SENTRY_DSN is empty")
+	}
+
+	if err := sentry.Init(sentry.ClientOptions{Dsn: dsn}); err != nil {
+		return func() {}, err
+	}
+
+	return func() { sentry.Flush(sentryTimeout) }, nil
 }
