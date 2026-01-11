3e832345da1332762009580fb24e3f194b1bfe55
IDilettant <dedenev.dev@gmail.com>
2026-01-11 21:06:49 +0100

    feat(domain): add link model, validation and domain errors

---
+++ internal/domain/errors.go
@@ -0,0 +1,10 @@
+package domain
+
+import "errors"
+
+var (
+	ErrNotFound          = errors.New("not found")
+	ErrInvalidURL        = errors.New("invalid url")
+	ErrInvalidShortName  = errors.New("invalid short name")
+	ErrShortNameConflict = errors.New("short name already exists")
+)
---
+++ internal/domain/validate.go
@@ -0,0 +1,40 @@
+package domain
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+var shortNameRe = regexp.MustCompile(`^[a-zA-Z0-9]{4,32}$`)
+
+func ValidateOriginalURL(s string) error {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return ErrInvalidURL
+	}
+
+	u, err := url.ParseRequestURI(s)
+	if err != nil {
+		return ErrInvalidURL
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return ErrInvalidURL
+	}
+
+	return nil
+}
+
+func ValidateShortName(s string) error {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return ErrInvalidShortName
+	}
+
+	if !shortNameRe.MatchString(s) {
+		return ErrInvalidShortName
+	}
+
+	return nil
+}
