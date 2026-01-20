1058b822ad8b0b2c3d38cefc5a4769213cc5920a
IDilettant <dedenev.dev@gmail.com>
2026-01-20 12:50:20 +0100

    feat(http): add engine plugin helpers for middleware setup

---
+++ internal/adapters/http/plugins/plugins.go
@@ -0,0 +1,47 @@
+package plugins
+
+import (
+	"time"
+
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+
+	"code/internal/adapters/http/middleware"
+)
+
+func Logger() func(*gin.Engine) {
+	return func(r *gin.Engine) {
+		r.Use(gin.Logger())
+	}
+}
+
+func Recovery() func(*gin.Engine) {
+	return func(r *gin.Engine) {
+		r.Use(middleware.Recovery())
+	}
+}
+
+func Sentry(timeout time.Duration) func(*gin.Engine) {
+	return func(r *gin.Engine) {
+		r.Use(sentrygin.New(sentrygin.Options{
+			Repanic: true,
+			Timeout: timeout,
+		}))
+	}
+}
+
+func RequestTimeout(d time.Duration) func(*gin.Engine) {
+	return func(r *gin.Engine) {
+		if d > 0 {
+			r.Use(middleware.RequestTimeout(d))
+		}
+	}
+}
+
+func CORS(origins []string) func(*gin.Engine) {
+	return func(r *gin.Engine) {
+		if len(origins) > 0 {
+			r.Use(middleware.CORS(origins))
+		}
+	}
+}
