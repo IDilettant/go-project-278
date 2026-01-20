64ce7e490fd0b2601305a192bf6b1ca03facc913
IDilettant <dedenev.dev@gmail.com>
2026-01-20 16:55:38 +0100

    refactor(links): allow structured logging in application layer

--- internal/app/links/logger.go
+++ internal/app/links/logger.go
@@ -2,10 +2,16 @@ package links
 
 // Logger is a minimal logging interface for the application layer.
 type Logger interface {
+	With(kv ...any) Logger
+	Info(msg string, kv ...any)
 	Warn(msg string, kv ...any)
+	Error(msg string, kv ...any)
 }
 
 // NopLogger drops all log messages.
 type NopLogger struct{}
 
-func (NopLogger) Warn(string, ...any) {}
+func (NopLogger) With(...any) Logger   { return NopLogger{} }
+func (NopLogger) Info(string, ...any)  {}
+func (NopLogger) Warn(string, ...any)  {}
+func (NopLogger) Error(string, ...any) {}
--- internal/assembly/apiapp/logadapter.go
+++ internal/assembly/apiapp/logadapter.go
@@ -10,12 +10,36 @@ type linksSlogLogger struct {
 	l *slog.Logger
 }
 
+func (l linksSlogLogger) With(kv ...any) links.Logger {
+	if l.l == nil {
+		return l
+	}
+
+	return linksSlogLogger{l: l.l.With(kv...)}
+}
+
+func (l linksSlogLogger) Info(msg string, kv ...any) {
+	if l.l == nil {
+		return
+	}
+
+	l.l.Info(msg, kv...)
+}
+
 func (l linksSlogLogger) Warn(msg string, kv ...any) {
 	if l.l == nil {
 		return
 	}
 
 	l.l.Warn(msg, kv...)
+}
+
+func (l linksSlogLogger) Error(msg string, kv ...any) {
+	if l.l == nil {
+		return
+	}
+
+	l.l.Error(msg, kv...)
 }
 
 var _ links.Logger = linksSlogLogger{}
